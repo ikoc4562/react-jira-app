63b296750f90771e75a7672de26a0efcaaf0e396
isakoc <ikoc4562@gmail.com>
2023-02-02 23:41:50 +0200

    Initial commit

---
+++ src/components/TaskCreate.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+
+function TaskCreate({ onCreate, task, taskformUpdate, onUpdate }) {
+  const [title, setTitle] = useState(task ? task.title : '');
+  const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : '');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleTaskChange = (event) => {
+    setTaskDesc(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (taskformUpdate) {
+      onUpdate(task.id, title, taskDesc);
+    } else {
+      if(title!=='' & taskDesc!==''){
+        onCreate(title, taskDesc);
+      } else
+      {
+        alert('Pls fill all the fields.');
+      }
+
+    }
+
+    setTitle('');
+    setTaskDesc('');
+  };
+
+  return (
+    <div>
+      {' '}
+      {taskformUpdate ? (
+        <div className="task-update">
+          <h3>Edit task</h3>
+          <form className="task-form">
+            <label className="task-label">New title</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="task-input"
+            />
+            <label className="task-label">New description</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="task-input"
+              rows={5}
+            />
+            <button
+              className="task-button update-button"
+              onClick={handleSubmit}
+            >
+              Update
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="task-create">
+          <h3>Add new task</h3>
+          <form className="task-form">
+            <label className="task-label">Title</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="task-input"
+            />
+            <label className="task-label">Description</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="task-input"
+              rows={5}
+            />
+            <button className="task-button" onClick={handleSubmit}>
+              Create
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TaskCreate;
